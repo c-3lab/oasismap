2611dd8442072cd4f80a8d098bd401371dec578d
hiromu-bit <hiromu0621@icloud.com>
2024-01-29 16:09:18 +0900

    折れ線グラフの表示を追加

--- Frontend/sample/src/app/page.tsx
+++ Frontend/sample/src/app/page.tsx
@@ -3,10 +3,11 @@ import dynamic from 'next/dynamic';
 import Image from 'next/image'
 import { DateTime } from 'luxon';
 import StarseekerFrontend from 'starseeker-frontend';
-import { AreaChart, Area, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
+import { AreaChart, Area, XAxis, YAxis, Tooltip, ResponsiveContainer, LineChart, Legend, Line } from 'recharts';
 import { BarChart, Bar, Cell } from 'recharts';
 import React, { useState } from 'react';
 import customData from './customData.json';
+import LineData from './LineData.json'
 
 
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
@@ -29,6 +30,7 @@ interface MyObject {
   q4: number;
   q5: number;
   q6: number;
+  total: number;
 }
 
 const pinData = customData.map(data => ({
@@ -37,20 +39,42 @@ const pinData = customData.map(data => ({
   title: `ピン${customData.indexOf(data) + 1}`, 
 }));
 
-
+// 積み上げグラフ用データ
 const oneYear = customData.map((obj) => ({
   ...obj,
   timestamp: Number(DateTime.fromISO(obj.timestamp).toFormat('MM')),
+  total: obj.q1 + obj.q2 + obj.q3 + obj.q4 + obj.q5 + obj.q6,
 }));
 
 const oneMonth = customData.map((obj) => ({
   ...obj,
   timestamp: Number(DateTime.fromISO(obj.timestamp).toFormat('dd')),
+  total: obj.q1 + obj.q2 + obj.q3 + obj.q4 + obj.q5 + obj.q6,
 }));
 
 const oneDay = customData.map((obj) => ({
   ...obj,
   timestamp: Number(DateTime.fromISO(obj.timestamp).toFormat('HH')),
+  total: obj.q1 + obj.q2 + obj.q3 + obj.q4 + obj.q5 + obj.q6,
+}));
+
+//折れ線グラフ用データ
+const oneYear3 = LineData.map((obj) => ({
+  ...obj,
+  timestamp: Number(DateTime.fromISO(obj.timestamp).toFormat('MM')),
+  total: obj.q1 + obj.q2 + obj.q3 + obj.q4 + obj.q5 + obj.q6,
+}));
+
+const oneMonth3 = LineData.map((obj) => ({
+  ...obj,
+  timestamp: Number(DateTime.fromISO(obj.timestamp).toFormat('dd')),
+  total: obj.q1 + obj.q2 + obj.q3 + obj.q4 + obj.q5 + obj.q6,
+}));
+
+const oneDay3 = LineData.map((obj) => ({
+  ...obj,
+  timestamp: Number(DateTime.fromISO(obj.timestamp).toFormat('HH')),
+  total: obj.q1 + obj.q2 + obj.q3 + obj.q4 + obj.q5 + obj.q6,
 }));
 
 function mergeWithTime(objects: MyObject[], start: number, end: number, currentTime: number): MyObject[] {
@@ -61,14 +85,23 @@ function mergeWithTime(objects: MyObject[], start: number, end: number, currentT
     const matchingObjects = sortedObjects.filter(obj => obj.timestamp === timestamp);
     if (matchingObjects.length > 0) {
       const totalQ1 = matchingObjects.reduce((acc, obj) => acc + obj.q1, 0);
+      const totalQ2 = matchingObjects.reduce((acc, obj) => acc + obj.q2, 0);
+      const totalQ3 = matchingObjects.reduce((acc, obj) => acc + obj.q3, 0);
+      const totalQ4 = matchingObjects.reduce((acc, obj) => acc + obj.q4, 0);
+      const totalQ5 = matchingObjects.reduce((acc, obj) => acc + obj.q5, 0);
+      const totalQ6 = matchingObjects.reduce((acc, obj) => acc + obj.q6, 0);
+      
+      const total = totalQ1 + totalQ2 + totalQ3 + totalQ4 + totalQ5 + totalQ6;
+
       result.push({ 
         timestamp, 
-        q1: matchingObjects.reduce((acc, obj) => acc + obj.q1, 0),
-        q2: matchingObjects.reduce((acc, obj) => acc + obj.q2, 0),
-        q3: matchingObjects.reduce((acc, obj) => acc + obj.q3, 0),
-        q4: matchingObjects.reduce((acc, obj) => acc + obj.q4, 0),
-        q5: matchingObjects.reduce((acc, obj) => acc + obj.q5, 0),
-        q6: matchingObjects.reduce((acc, obj) => acc + obj.q6, 0),
+        q1: totalQ1,
+        q2: totalQ2,
+        q3: totalQ3,
+        q4: totalQ4,
+        q5: totalQ5,
+        q6: totalQ6,
+        total: total,
       });
     } else {
       result.push({ 
@@ -79,6 +112,7 @@ function mergeWithTime(objects: MyObject[], start: number, end: number, currentT
         q4: 0,
         q5: 0,
         q6: 0,
+        total: 0,
       });
     }
   }
@@ -89,12 +123,20 @@ function mergeWithTime(objects: MyObject[], start: number, end: number, currentT
 
   return result;
 }
+
 const oneDay2 = mergeWithTime(oneDay, 0, 23, now.hour);
 const oneMonth2 = mergeWithTime(oneMonth, 1, 31, now.day);
 const oneYear2 = mergeWithTime(oneYear, 1, 12, now.month);
 
 console.log("整形前", oneDay.filter(item => item.timestamp))
 console.log("整形後", oneDay2.filter(item => item.timestamp))
+
+const oneDay4 = mergeWithTime(oneDay3, 0, 23, now.hour);
+const oneMonth4 = mergeWithTime(oneMonth3, 1, 31, now.day);
+const oneYear4 = mergeWithTime(oneYear3, 1, 12, now.month);
+
+console.log("整形前", oneDay3.filter(item => item.timestamp))
+console.log("整形後", oneDay4.filter(item => item.timestamp))
 
 // 積立棒グラフ
 const BPlot = (props: any) => {
@@ -120,6 +162,25 @@ const BPlot = (props: any) => {
   );
 };
 
+// 折れ線グラフ
+const LPlot = (props: any) => {
+  const { title, plotdata, xTickFormatter } = props;
+
+  return (
+    <>
+      <h3 className="text-white text-center">{title}</h3>
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart width={730} height={250} data={plotdata} margin={{ top: 10, right: 30, left: 0, bottom: 0}}>
+          <XAxis dataKey="timestamp" tick={xTickFormatter} interval={0} />
+          <YAxis tickCount={7} domain={[0, 6]} />
+          <Tooltip />
+          <Legend verticalAlign='bottom' />
+          <Line type="monotone" dataKey="total" stroke={COLORS[1]} dot={true} /> 
+        </LineChart>
+      </ResponsiveContainer>
+    </>
+  );
+};
 
 // or
 //
@@ -189,7 +250,7 @@ export default function Home() {
 
       {/* Container */}
       <div className="w-full h-96 lg:w-1/2 xl:w-1/2 p-4 lg:p-8 bg-gray-200">
-        <h1 className="text-2xl font-bold">Container ({viewMode})</h1>
+        <h1 className="text-2xl font-bold">積み上げ棒グラフ ({viewMode})</h1>
         <div className="h-[300px] bg-gray-700 rounded">
           {/* BPlotコンポーネントのplotdataを表示モードに基づいて選択 */}
           {viewMode === '時間' && (
@@ -199,6 +260,43 @@ export default function Home() {
             <BPlot plotdata={oneMonth2} title="日" xTickFormatter={renderCustomMonthTick} />
           )}
           {viewMode === '月' && <BPlot plotdata={oneYear2} title="月" />}
+        </div>
+      </div>
+
+      {/* ボタンで表示モードを切り替える */}
+      <div className="flex mt-4 space-x-4">
+        <button
+          onClick={() => handleViewModeChange('月')}
+          className={`px-4 py-2 ${viewMode === '月' ? 'bg-blue-500 text-white' : 'bg-gray-300'}`}
+        >
+          月
+        </button>
+        <button
+          onClick={() => handleViewModeChange('日')}
+          className={`px-4 py-2 ${viewMode === '日' ? 'bg-blue-500 text-white' : 'bg-gray-300'}`}
+        >
+          日
+        </button>
+        <button
+          onClick={() => handleViewModeChange('時間')}
+          className={`px-4 py-2 ${viewMode === '時間' ? 'bg-blue-500 text-white' : 'bg-gray-300'}`}
+        >
+          時間
+        </button>
+      </div>
+
+      {/* Container */}
+      <div className="w-full h-96 lg:w-1/2 xl:w-1/2 p-4 lg:p-8 bg-gray-200">
+        <h1 className="text-2xl font-bold">折れ線グラフ ({viewMode})</h1>
+        <div className="h-[300px] bg-gray-700 rounded">
+          {/* BPlotコンポーネントのplotdataを表示モードに基づいて選択 */}
+          {viewMode === '時間' && (
+            <LPlot plotdata={oneDay4} title="時間" xTickFormatter={renderCustomDayTick} />
+          )}
+          {viewMode === '日' && (
+            <LPlot plotdata={oneMonth4} title="日" xTickFormatter={renderCustomMonthTick} />
+          )}
+          {viewMode === '月' && <LPlot plotdata={oneYear4} title="月" />}
         </div>
       </div>
 
