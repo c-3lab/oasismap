85584806afc5ec9f085fab85cb8f248253c2cc5e
hiromu-bit <hiromu0621@icloud.com>
2024-02-05 11:17:06 +0900

    JSONファイル内の緯度経度情報をMapcomponetに渡す様に修正

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,18 @@
 'use client'
 import dynamic from 'next/dynamic'
 import styles from './page.module.css'
+import customData from './customData.json'
 
 const Map = dynamic(
   () => import('starseeker-frontend').then((module) => module.Map),
   { ssr: false }
 )
+
+const pinData = customData.map((data) => ({
+  latitude: data.latitude,
+  longitude: data.longitude,
+  title: `ピン${customData.indexOf(data) + 1}`,
+}))
 
 export default function Home() {
   return (
@@ -17,7 +24,7 @@ export default function Home() {
         pointEntities={[]}
         surfaceEntities={[]}
         fiware={{ servicePath: '', tenant: '' }}
-        pinData={[]}
+        pinData={pinData}
       />
     </main>
   )
