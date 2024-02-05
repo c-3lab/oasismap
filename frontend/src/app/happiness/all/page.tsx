ff17d3d9235e15d68ce550f824f83ac26441b2fe
Kazuki Urabe <urabe0225@gmail.com>
2024-02-05 16:22:21 +0900

    グラフを挿入

--- frontend/src/app/happiness/all/page.tsx
+++ frontend/src/app/happiness/all/page.tsx
@@ -22,6 +22,141 @@ import {
 } from '@/components/fields/date-time-textbox'
 import { PeriodType } from '@/types/period'
 
+import LineData from './LineData.json'
+import { DateTime } from 'luxon'
+
+import { LPlot } from '@/components/happiness/graph'
+import { ResponsiveContainer } from 'recharts'
+
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink']
+
+interface happinessObj {
+  timestamp: any
+  q1: number
+  q2: number
+  q3: number
+  q4: number
+  q5: number
+  q6: number
+  averageQ1: number
+  averageQ2: number
+  averageQ3: number
+  averageQ4: number
+  averageQ5: number
+  averageQ6: number
+}
+
+function mergeWithTime(
+  objects: {
+    timestamp: number
+    latitude: number
+    longitude: number
+    q1: number
+    q2: number
+    q3: number
+    q4: number
+    q5: number
+    q6: number
+  }[],
+  start: number,
+  end: number,
+  currentTime: number
+): happinessObj[] {
+  const sortedObjects = objects.sort((a, b) => a.timestamp - b.timestamp)
+  const result: happinessObj[] = []
+
+  for (let timestamp = start; timestamp <= end; timestamp++) {
+    const matchingObjects = sortedObjects.filter(
+      (obj) => obj.timestamp === timestamp
+    )
+    if (matchingObjects.length > 0) {
+      const Q1 = matchingObjects.reduce((acc, obj) => acc + obj.q1, 0)
+      const Q2 = matchingObjects.reduce((acc, obj) => acc + obj.q2, 0)
+      const Q3 = matchingObjects.reduce((acc, obj) => acc + obj.q3, 0)
+      const Q4 = matchingObjects.reduce((acc, obj) => acc + obj.q4, 0)
+      const Q5 = matchingObjects.reduce((acc, obj) => acc + obj.q5, 0)
+      const Q6 = matchingObjects.reduce((acc, obj) => acc + obj.q6, 0)
+
+      const averageQ1 = (Q1 / matchingObjects.length).toFixed(1)
+      const averageQ2 = (Q2 / matchingObjects.length).toFixed(1)
+      const averageQ3 = (Q3 / matchingObjects.length).toFixed(1)
+      const averageQ4 = (Q4 / matchingObjects.length).toFixed(1)
+      const averageQ5 = (Q5 / matchingObjects.length).toFixed(1)
+      const averageQ6 = (Q6 / matchingObjects.length).toFixed(1)
+
+      result.push({
+        timestamp,
+        q1: Q1,
+        q2: Q2,
+        q3: Q3,
+        q4: Q4,
+        q5: Q5,
+        q6: Q6,
+        averageQ1: parseFloat(averageQ1),
+        averageQ2: parseFloat(averageQ2),
+        averageQ3: parseFloat(averageQ3),
+        averageQ4: parseFloat(averageQ4),
+        averageQ5: parseFloat(averageQ5),
+        averageQ6: parseFloat(averageQ6),
+      })
+    } else {
+      result.push({
+        timestamp,
+        q1: 0,
+        q2: 0,
+        q3: 0,
+        q4: 0,
+        q5: 0,
+        q6: 0,
+        averageQ1: 0,
+        averageQ2: 0,
+        averageQ3: 0,
+        averageQ4: 0,
+        averageQ5: 0,
+        averageQ6: 0,
+      })
+    }
+  }
+  const currentIndex = result.findIndex((obj) => obj.timestamp === currentTime)
+  if (currentIndex !== -1) {
+    result.unshift(...result.splice(currentIndex, result.length - currentIndex))
+  }
+
+  return result
+}
+
+const now = DateTime.local()
+//折れ線グラフ用データ
+const ourHappiness = [
+  mergeWithTime(
+    LineData.map((obj) => ({
+      ...obj,
+      timestamp: Number(DateTime.fromISO(obj.timestamp).toFormat('MM')),
+    })),
+    1,
+    12,
+    now.month
+  ),
+  mergeWithTime(
+    LineData.map((obj) => ({
+      ...obj,
+      timestamp: Number(DateTime.fromISO(obj.timestamp).toFormat('dd')),
+    })),
+    1,
+    31,
+    now.day
+  ),
+  mergeWithTime(
+    LineData.map((obj) => ({
+      ...obj,
+      timestamp: Number(DateTime.fromISO(obj.timestamp).toFormat('HH')),
+    })),
+    0,
+    23,
+    now.hour
+  ),
+]
+
 const HappinessAll: React.FC = () => {
   const [period, setPeriod] = useState(PeriodType.Month)
 
@@ -33,6 +168,19 @@ const HappinessAll: React.FC = () => {
     date: '2024-01-27',
     time: '12:00',
   })
+
+  const renderCustomDayTick = (tickProps: any) => {
+    const { x, y, payload } = tickProps
+    const hour = payload.value
+    if (hour % 2 === 0) {
+      return (
+        <text x={x} y={y} dy={16} fill="#666" textAnchor="middle">
+          {hour}
+        </text>
+      )
+    }
+    return null
+  }
 
   return (
     <Grid container>
@@ -70,6 +218,14 @@ const HappinessAll: React.FC = () => {
           }}
         >
           グラフ表示エリア
+          <ResponsiveContainer width="100%" height={300}>
+            <LPlot
+              plotdata={ourHappiness[2]}
+              title="時間"
+              color={COLORS}
+              xTickFormatter={renderCustomDayTick}
+            />
+          </ResponsiveContainer>
         </Grid>
         <Grid
           container
---
+++ frontend/src/components/happiness/graph.tsx
@@ -0,0 +1,65 @@
+import { XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts'
+import { LineChart, Legend, Line } from 'recharts'
+
+export const LPlot = (props: any) => {
+  const { title, plotdata, color, xTickFormatter } = props
+
+  return (
+    <>
+      <h3 className="text-white text-center">{title}</h3>
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          width={730}
+          height={250}
+          data={plotdata}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+        >
+          <XAxis dataKey="timestamp" tick={xTickFormatter} interval={0} />
+          <YAxis
+            tickCount={11}
+            domain={[0, 1]}
+            ticks={[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]}
+          />
+          <Tooltip />
+          <Legend verticalAlign="bottom" />
+          <Line
+            type="monotone"
+            dataKey="averageQ1"
+            stroke={color[0]}
+            dot={true}
+          />
+          <Line
+            type="monotone"
+            dataKey="averageQ2"
+            stroke={color[1]}
+            dot={true}
+          />
+          <Line
+            type="monotone"
+            dataKey="averageQ3"
+            stroke={color[2]}
+            dot={true}
+          />
+          <Line
+            type="monotone"
+            dataKey="averageQ4"
+            stroke={color[3]}
+            dot={true}
+          />
+          <Line
+            type="monotone"
+            dataKey="averageQ5"
+            stroke={color[4]}
+            dot={true}
+          />
+          <Line
+            type="monotone"
+            dataKey="averageQ6"
+            stroke={color[5]}
+            dot={true}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </>
+  )
+}
