6070885a4a68dc466459e296bb468982dc3c5e10
y-arashi <y-arashi@abelsoft.co.jp>
2024-02-07 11:59:29 +0900

    利用者の幸福度画面のワイヤーフレーム作成 (#15)

---
+++ frontend/src/app/happiness/me/page.tsx
@@ -0,0 +1,125 @@
+'use client'
+import { useState } from 'react'
+import { Button, ButtonGroup, Grid } from '@mui/material'
+
+import Map from '@/components/happiness/map'
+import {
+  DateTimeTextbox,
+  useDateTime,
+} from '@/components/fields/date-time-textbox'
+import { PeriodType } from '@/types/period'
+
+const HappinessMe: React.FC = () => {
+  const [period, setPeriod] = useState(PeriodType.Month)
+
+  const startDateTimeProps = useDateTime({
+    date: '2024-01-26',
+    time: '09:00',
+  })
+  const endDateTimeProps = useDateTime({
+    date: '2024-01-27',
+    time: '12:00',
+  })
+
+  return (
+    <Grid container>
+      <Grid
+        container
+        item
+        xs={12}
+        md={6}
+        sx={{ height: { xs: '50vh', md: 'calc(100vh - 64px)' } }}
+      >
+        <Map
+          pointEntities={[]}
+          surfaceEntities={[]}
+          fiware={{ servicePath: '', tenant: '' }}
+        />
+      </Grid>
+      <Grid
+        container
+        item
+        xs={12}
+        md={6}
+        rowSpacing={4}
+        columnSpacing={1}
+        justifyContent={'center'}
+        sx={{ px: { md: '16px' }, my: { xs: '32px', md: 0 } }}
+      >
+        <Grid
+          item
+          xs={12}
+          md={12}
+          sx={{
+            backgroundColor: '#a0a0a0',
+            minHeight: '300px',
+          }}
+        >
+          グラフ表示エリア
+        </Grid>
+        <Grid
+          container
+          justifyContent="center"
+          rowSpacing={4}
+          columnSpacing={1}
+          sx={{ px: { xs: '16px', md: 0 }, my: { xs: '0px' } }}
+        >
+          <Grid item xs={12} md={12} lg={8}>
+            <ButtonGroup size="large" aria-label="large button group" fullWidth>
+              <Button
+                key="month"
+                variant={period === PeriodType.Month ? 'contained' : 'outlined'}
+                onClick={() => setPeriod(PeriodType.Month)}
+              >
+                月
+              </Button>
+              <Button
+                key="day"
+                variant={period === PeriodType.Day ? 'contained' : 'outlined'}
+                onClick={() => setPeriod(PeriodType.Day)}
+              >
+                日
+              </Button>
+              <Button
+                key="time"
+                variant={period === PeriodType.Time ? 'contained' : 'outlined'}
+                onClick={() => setPeriod(PeriodType.Time)}
+              >
+                時間
+              </Button>
+            </ButtonGroup>
+          </Grid>
+          <Grid item xs={12} md={12} lg={8}>
+            <DateTimeTextbox
+              dateLabel="開始日"
+              timeLabel="時間"
+              {...startDateTimeProps}
+            />
+          </Grid>
+          <Grid item xs={12} md={12} lg={8}>
+            <DateTimeTextbox
+              dateLabel="終了日"
+              timeLabel="時間"
+              {...endDateTimeProps}
+            />
+          </Grid>
+          <Grid container item xs={12} md={12} lg={8} columnSpacing={1}>
+            <Grid item xs={8} md={8} />
+            <Grid item xs={4} md={4}>
+              <Button variant="outlined" color="secondary" fullWidth>
+                検索
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} md={12} lg={8}>
+            <Button variant="contained" color="primary" size="large" fullWidth>
+              幸福度を入力
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default HappinessMe
