75c90dd10428dac57fb2204078ca78bf5672894a
Urabe Kazuki <urabe0225@gmail.com>
2024-02-09 18:26:06 +0900

    グラフ追加及びピン表示の追加 (#18)
    * submoduleの変更
    
    * page.tsxの修正
    
    * Dataファイルをfrontend/srcに追加
    
    * JSONファイル内の緯度経度情報をMapcomponetに渡す様に修正
    
    * frontendにluxon,rechartsを追加
    
    * グラフを挿入
    
    * oasismap内に地図コンポーネントを作成
    
    * Submoduleの更新
    
    * グラフのボタン切替可能に変更 (#13)
    
    * LineData.jsonのデータ追加
    
    * ボタンでのグラフの表示切り替え可能に変更
    
    * currentGraphIndexは不要
    
    * /happiness/meにグラフとピンの表示追加 (#19)
    
    * 全体の幸福度のPinDataの参照先の変更
    
    * 個人幸福度のpage.tsxの作成
    
    * graphファイルに棒グラフのコンテナを作成
    
    * グラフの背景色を白に変更 (#20)
    
    * 仮データの修正 (#22)
    
    * ピンの色を幸福度毎に変化 (#23)
    
    * グラフの背景色を白に変更
    
    * 質問毎にピンの色を変化するように変更
    
    * 幸福度を識別する為のtypeを追加
    
    * pinDataにtypeを追加
    
    * ダミーバックエンドの作成 (#24)
    
    * ダミーバックエンドの追加
    
    * データ構造の修正
    
    * データ構造の修正
    
    * ダミーバックエンドのDocker環境を作成
    
    * 地図メニューを追加(利用者画面)
    
    * 地図メニューを追加(全体画面)
    
    * 仮データ構造の修正
    
    * 仮データの修正
    
    * 幸福度が0のピンは表示しないように修正
    
    ---------
    
    Co-authored-by: hiromu-bit <hiromu0621@icloud.com>
    Co-authored-by: Hiromu Obata <140672122+hiromu-bit@users.noreply.github.com>

--- frontend/src/app/happiness/me/page.tsx
+++ frontend/src/app/happiness/me/page.tsx
@@ -1,13 +1,144 @@
 'use client'
+import dynamic from 'next/dynamic'
 import { useState } from 'react'
 import { Button, ButtonGroup, Grid } from '@mui/material'
-
-import Map from '@/components/happiness/map'
+import { PeriodType } from '@/types/period'
+import { DateTime } from 'luxon'
+import { ResponsiveContainer } from 'recharts'
+//import Map from '@/components/happiness/map'
+const MapSet = dynamic(() => import('@/components/map/mapset'), { ssr: false })
+import { GetPin, COLORS } from '@/components/utils/pin'
 import {
   DateTimeTextbox,
   useDateTime,
 } from '@/components/fields/date-time-textbox'
-import { PeriodType } from '@/types/period'
+
+import { BPlot } from '@/components/happiness/graph'
+import data from './myHappiness.json'
+
+const pinData = GetPin(data)
+
+interface happinessObj {
+  timestamp: any
+  happiness1: number
+  happiness2: number
+  happiness3: number
+  happiness4: number
+  happiness5: number
+  happiness6: number
+}
+
+function mergeWithTime(
+  objects: {
+    timestamp: number
+    latitude: number
+    longitude: number
+    answers: {
+      happiness1: number
+      happiness2: number
+      happiness3: number
+      happiness4: number
+      happiness5: number
+      happiness6: number
+    }
+  }[],
+  start: number,
+  end: number,
+  currentTime: number
+): happinessObj[] {
+  const sortedObjects = objects.sort((a, b) => a.timestamp - b.timestamp)
+  const result: happinessObj[] = []
+
+  for (let timestamp = start; timestamp <= end; timestamp++) {
+    const matchingObjects = sortedObjects.filter(
+      (obj) => obj.timestamp === timestamp
+    )
+    if (matchingObjects.length > 0) {
+      const Q1 = matchingObjects.reduce(
+        (acc, obj) => acc + obj.answers.happiness1,
+        0
+      )
+      const Q2 = matchingObjects.reduce(
+        (acc, obj) => acc + obj.answers.happiness2,
+        0
+      )
+      const Q3 = matchingObjects.reduce(
+        (acc, obj) => acc + obj.answers.happiness3,
+        0
+      )
+      const Q4 = matchingObjects.reduce(
+        (acc, obj) => acc + obj.answers.happiness4,
+        0
+      )
+      const Q5 = matchingObjects.reduce(
+        (acc, obj) => acc + obj.answers.happiness5,
+        0
+      )
+      const Q6 = matchingObjects.reduce(
+        (acc, obj) => acc + obj.answers.happiness6,
+        0
+      )
+
+      result.push({
+        timestamp,
+        happiness1: Q1,
+        happiness2: Q2,
+        happiness3: Q3,
+        happiness4: Q4,
+        happiness5: Q5,
+        happiness6: Q6,
+      })
+    } else {
+      result.push({
+        timestamp,
+        happiness1: 0,
+        happiness2: 0,
+        happiness3: 0,
+        happiness4: 0,
+        happiness5: 0,
+        happiness6: 0,
+      })
+    }
+  }
+  const currentIndex = result.findIndex((obj) => obj.timestamp === currentTime)
+  if (currentIndex !== -1) {
+    result.unshift(...result.splice(currentIndex, result.length - currentIndex))
+  }
+
+  return result
+}
+
+const now = DateTime.local()
+//積み上げ棒グラフ用データ
+const MyHappiness = [
+  mergeWithTime(
+    data.map((obj) => ({
+      ...obj,
+      timestamp: Number(DateTime.fromISO(obj.timestamp).toFormat('MM')),
+    })),
+    1,
+    12,
+    now.month
+  ),
+  mergeWithTime(
+    data.map((obj) => ({
+      ...obj,
+      timestamp: Number(DateTime.fromISO(obj.timestamp).toFormat('dd')),
+    })),
+    1,
+    31,
+    now.day
+  ),
+  mergeWithTime(
+    data.map((obj) => ({
+      ...obj,
+      timestamp: Number(DateTime.fromISO(obj.timestamp).toFormat('HH')),
+    })),
+    0,
+    23,
+    now.hour
+  ),
+]
 
 const HappinessMe: React.FC = () => {
   const [period, setPeriod] = useState(PeriodType.Month)
@@ -21,6 +152,19 @@ const HappinessMe: React.FC = () => {
     time: '12:00',
   })
 
+  const renderCustomDayTick = (tickProps: any) => {
+    const { x, y, payload } = tickProps
+    const hour = payload.value
+    if (hour % 2 === 0) {
+      return (
+        <text x={x} y={y} dy={16} fill="#666" textAnchor="middle">
+          {hour}
+        </text>
+      )
+    }
+    return null
+  }
+
   return (
     <Grid container>
       <Grid
@@ -30,10 +174,11 @@ const HappinessMe: React.FC = () => {
         md={6}
         sx={{ height: { xs: '50vh', md: 'calc(100vh - 64px)' } }}
       >
-        <Map
+        <MapSet
           pointEntities={[]}
           surfaceEntities={[]}
           fiware={{ servicePath: '', tenant: '' }}
+          pinData={pinData}
         />
       </Grid>
       <Grid
@@ -51,11 +196,19 @@ const HappinessMe: React.FC = () => {
           xs={12}
           md={12}
           sx={{
-            backgroundColor: '#a0a0a0',
+            backgroundColor: '#FFFFFF',
             minHeight: '300px',
           }}
         >
           グラフ表示エリア
+          <ResponsiveContainer width="100%" height={300}>
+            <BPlot
+              plotdata={MyHappiness[period]}
+              title="時間"
+              color={COLORS}
+              xTickFormatter={renderCustomDayTick}
+            />
+          </ResponsiveContainer>
         </Grid>
         <Grid
           container
@@ -69,21 +222,27 @@ const HappinessMe: React.FC = () => {
               <Button
                 key="month"
                 variant={period === PeriodType.Month ? 'contained' : 'outlined'}
-                onClick={() => setPeriod(PeriodType.Month)}
+                onClick={() => {
+                  setPeriod(PeriodType.Month)
+                }}
               >
                 月
               </Button>
               <Button
                 key="day"
                 variant={period === PeriodType.Day ? 'contained' : 'outlined'}
-                onClick={() => setPeriod(PeriodType.Day)}
+                onClick={() => {
+                  setPeriod(PeriodType.Day)
+                }}
               >
                 日
               </Button>
               <Button
                 key="time"
                 variant={period === PeriodType.Time ? 'contained' : 'outlined'}
-                onClick={() => setPeriod(PeriodType.Time)}
+                onClick={() => {
+                  setPeriod(PeriodType.Time)
+                }}
               >
                 時間
               </Button>
---
+++ frontend/src/components/happiness/graph.tsx
@@ -0,0 +1,96 @@
+import { XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts'
+import { BarChart, Bar, LineChart, Legend, Line } from 'recharts'
+
+export const LPlot = (props: any) => {
+  const { title, plotdata, color, xTickFormatter } = props
+
+  return (
+    <>
+      <h3 className="text-white text-center">{title}</h3>
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          width={730}
+          height={250}
+          data={plotdata}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+          style={{ backgroundColor: '#FFFFFF' }}
+        >
+          <XAxis dataKey="timestamp" tick={xTickFormatter} interval={0} />
+          <YAxis
+            tickCount={11}
+            domain={[0, 1]}
+            ticks={[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]}
+          />
+          <Tooltip />
+          <Legend verticalAlign="bottom" />
+          <Line
+            type="monotone"
+            dataKey="averageQ1"
+            stroke={color[0]}
+            dot={true}
+          />
+          <Line
+            type="monotone"
+            dataKey="averageQ2"
+            stroke={color[1]}
+            dot={true}
+          />
+          <Line
+            type="monotone"
+            dataKey="averageQ3"
+            stroke={color[2]}
+            dot={true}
+          />
+          <Line
+            type="monotone"
+            dataKey="averageQ4"
+            stroke={color[3]}
+            dot={true}
+          />
+          <Line
+            type="monotone"
+            dataKey="averageQ5"
+            stroke={color[4]}
+            dot={true}
+          />
+          <Line
+            type="monotone"
+            dataKey="averageQ6"
+            stroke={color[5]}
+            dot={true}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </>
+  )
+}
+
+export const BPlot = (props: any) => {
+  const { title, plotdata, color, xTickFormatter } = props
+
+  return (
+    <>
+      <h3 className="text-white text-center">{title}</h3>
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          width={730}
+          height={250}
+          data={plotdata}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+          style={{ backgroundColor: '#FFFFFF' }}
+        >
+          <XAxis dataKey="timestamp" tick={xTickFormatter} interval={0} />
+          <YAxis />
+          <Tooltip />
+          <Legend verticalAlign="bottom" />
+          <Bar dataKey="happiness1" stackId={1} fill={color[0]} />
+          <Bar dataKey="happiness2" stackId={1} fill={color[1]} />
+          <Bar dataKey="happiness3" stackId={1} fill={color[2]} />
+          <Bar dataKey="happiness4" stackId={1} fill={color[3]} />
+          <Bar dataKey="happiness5" stackId={1} fill={color[4]} />
+          <Bar dataKey="happiness6" stackId={1} fill={color[5]} />
+        </BarChart>
+      </ResponsiveContainer>
+    </>
+  )
+}
