880ce618060945461680926e90c91933b24642f3
hiromu-bit <hiromu0621@icloud.com>
2024-02-13 17:38:27 +0900

    ピンのポップを回答時間と各質問の回答に変更

--- frontend/src/components/utils/pin.ts
+++ frontend/src/components/utils/pin.ts
@@ -10,10 +10,10 @@ export const COLORS = [
 export function GetPin(arr: any[]): any[] {
   return arr
     .filter((data) => data.answers[data.type] !== 0)
-    .map((data, index) => ({
+    .map((data) => ({
       type: data.type,
       latitude: data.location.value.coordinates[0],
       longitude: data.location.value.coordinates[1],
-      title: `ピン${index + 1}`,
+      title: `回答時間: ${new Date(data.timestamp).toLocaleString('ja-JP')}\n幸福度1: ${data.answers['happiness1']}\n幸福度2: ${data.answers['happiness2']}\n幸福度3: ${data.answers['happiness3']}\n幸福度4: ${data.answers['happiness4']}\n幸福度5: ${data.answers['happiness5']}\n幸福度6: ${data.answers['happiness6']}`,
     }))
 }
