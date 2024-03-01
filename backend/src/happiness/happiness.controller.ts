37377505ca7b07426781f20c943ff4eca0f99a3e
i-kaneko-abel <116322505+i-kaneko-abel@users.noreply.github.com>
2024-03-01 22:05:13 +0900

    [demo][2-3] 幸福度入力API作成 (#61)
    * バックエンドにKEYCLOAK_CLIENT_ISSUER追加
    
    * 幸福度入力API作成
    
    * 利用者幸福度認証修正
    
    * 認証ダミー関数削除

--- backend/src/happiness/happiness.controller.ts
+++ backend/src/happiness/happiness.controller.ts
@@ -1,21 +1,43 @@
-import { Controller, Get, Headers, Query } from '@nestjs/common';
+import { Body, Controller, Get, Headers, Post, Query } from '@nestjs/common';
 import { HappinessService } from './happiness.service';
 import { HappinessMeResponse } from './interface/happiness-me.response';
 import { GetHappinessMeDto } from './dto/get-happiness-me.dto';
 import { HappinessAllResponse } from './interface/happiness-all.response';
 import { GetHappinessAllDto } from './dto/get-happiness-all.dto';
+import { AuthService } from 'src/auth/auth';
+import { CreateHappinessDto } from './dto/create-happiness.dto';
+import { HappinessResponse } from './interface/happiness.response';
 
 @Controller('/api/happiness')
 export class HappinessController {
-  constructor(private readonly happinessService: HappinessService) {}
+  constructor(
+    private readonly happinessService: HappinessService,
+    private readonly authService: AuthService,
+  ) {}
+
+  @Post()
+  async createHappiness(
+    @Headers('Authorization') authorization: string,
+    @Body() createHappinessDto: CreateHappinessDto,
+  ): Promise<HappinessResponse> {
+    const userAttribute =
+      this.authService.getUserAttributesFromAuthorization(authorization);
+
+    return this.happinessService.postHappiness(
+      await userAttribute,
+      createHappinessDto,
+    );
+  }
 
   @Get('/me')
   async getHapinessMe(
     @Headers('Authorization') authorization: string,
     @Query() getHappinessMeDto: GetHappinessMeDto,
   ): Promise<HappinessMeResponse[]> {
+    const userAttribute =
+      this.authService.getUserAttributesFromAuthorization(authorization);
     return this.happinessService.findHapinessMe(
-      authorization,
+      (await userAttribute).nickname,
       getHappinessMeDto.start,
       getHappinessMeDto.end,
     );
