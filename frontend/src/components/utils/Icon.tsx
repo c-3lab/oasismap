f826e07f64b88007fb3983bd5329bd4a5211058b
Kazuki Urabe <urabe0225@gmail.com>
2024-05-27 17:26:14 +0900

    ピン周りのコード整理

--- frontend/src/components/utils/Icon.tsx
+++ frontend/src/components/utils/Icon.tsx
@@ -1,243 +1,48 @@
-import {
-  redIcon,
-  red9Icon,
-  red8Icon,
-  red7Icon,
-  red6Icon,
-  red5Icon,
-  red4Icon,
-  red3Icon,
-  red2Icon,
-  red1Icon,
-  blueIcon,
-  blue9Icon,
-  blue8Icon,
-  blue7Icon,
-  blue6Icon,
-  blue5Icon,
-  blue4Icon,
-  blue3Icon,
-  blue2Icon,
-  blue1Icon,
-  greenIcon,
-  green9Icon,
-  green8Icon,
-  green7Icon,
-  green6Icon,
-  green5Icon,
-  green4Icon,
-  green3Icon,
-  green2Icon,
-  green1Icon,
-  yellowIcon,
-  yellow9Icon,
-  yellow8Icon,
-  yellow7Icon,
-  yellow6Icon,
-  yellow5Icon,
-  yellow4Icon,
-  yellow3Icon,
-  yellow2Icon,
-  yellow1Icon,
-  orangeIcon,
-  orange9Icon,
-  orange8Icon,
-  orange7Icon,
-  orange6Icon,
-  orange5Icon,
-  orange4Icon,
-  orange3Icon,
-  orange2Icon,
-  orange1Icon,
-  violetIcon,
-  violet9Icon,
-  violet8Icon,
-  violet7Icon,
-  violet6Icon,
-  violet5Icon,
-  violet4Icon,
-  violet3Icon,
-  violet2Icon,
-  violet1Icon,
-} from './marker'
+import { createColoredIcon } from './marker'
+import { colorSet } from '@/theme/color'
 
 export const getIconByType = (type: string, answer: number) => {
   switch (type) {
     case 'happiness1':
-      return getIconForHappiness1(answer)
+      return getIconForHappiness(answer, colorSet["BLUE"])
     case 'happiness2':
-      return getIconForHappiness2(answer)
+      return getIconForHappiness(answer, colorSet["GREEN"])
     case 'happiness3':
-      return getIconForHappiness3(answer)
+      return getIconForHappiness(answer, colorSet["VIOLET"])
     case 'happiness4':
-      return getIconForHappiness4(answer)
+      return getIconForHappiness(answer, colorSet["YELLOW"])
     case 'happiness5':
-      return getIconForHappiness5(answer)
+      return getIconForHappiness(answer, colorSet["ORANGE"])
     case 'happiness6':
-      return getIconForHappiness6(answer)
+      return getIconForHappiness(answer, colorSet["RED"])
     default:
-      return redIcon
+      return getIconForHappiness(1, colorSet["RED"])
   }
 }
 
-const getIconForHappiness1 = (answer: number) => {
+const getIconForHappiness = (answer: number, palette: string[]) => {
   switch (answer) {
     case 1:
-      return blueIcon
+      return createColoredIcon(palette[0])
     case 0.9:
-      return blue9Icon
+      return createColoredIcon(palette[9])
     case 0.8:
-      return blue8Icon
+      return createColoredIcon(palette[8])
     case 0.7:
-      return blue7Icon
+      return createColoredIcon(palette[7])
     case 0.6:
-      return blue6Icon
+      return createColoredIcon(palette[6])
     case 0.5:
-      return blue5Icon
+      return createColoredIcon(palette[5])
     case 0.4:
-      return blue4Icon
+      return createColoredIcon(palette[4])
     case 0.3:
-      return blue3Icon
+      return createColoredIcon(palette[3])
     case 0.2:
-      return blue2Icon
+      return createColoredIcon(palette[2])
     case 0.1:
-      return blue1Icon
+      return createColoredIcon(palette[1])
     default:
-      return blueIcon
-  }
-}
-
-const getIconForHappiness2 = (answer: number) => {
-  switch (answer) {
-    case 1:
-      return greenIcon
-    case 0.9:
-      return green9Icon
-    case 0.8:
-      return green8Icon
-    case 0.7:
-      return green7Icon
-    case 0.6:
-      return green6Icon
-    case 0.5:
-      return green5Icon
-    case 0.4:
-      return green4Icon
-    case 0.3:
-      return green3Icon
-    case 0.2:
-      return green2Icon
-    case 0.1:
-      return green1Icon
-    default:
-      return greenIcon
-  }
-}
-
-const getIconForHappiness3 = (answer: number) => {
-  switch (answer) {
-    case 1:
-      return violetIcon
-    case 0.9:
-      return violet9Icon
-    case 0.8:
-      return violet8Icon
-    case 0.7:
-      return violet7Icon
-    case 0.6:
-      return violet6Icon
-    case 0.5:
-      return violet5Icon
-    case 0.4:
-      return violet4Icon
-    case 0.3:
-      return violet3Icon
-    case 0.2:
-      return violet2Icon
-    case 0.1:
-      return violet1Icon
-    default:
-      return violetIcon
-  }
-}
-
-const getIconForHappiness4 = (answer: number) => {
-  switch (answer) {
-    case 1:
-      return yellowIcon
-    case 0.9:
-      return yellow9Icon
-    case 0.8:
-      return yellow8Icon
-    case 0.7:
-      return yellow7Icon
-    case 0.6:
-      return yellow6Icon
-    case 0.5:
-      return yellow5Icon
-    case 0.4:
-      return yellow4Icon
-    case 0.3:
-      return yellow3Icon
-    case 0.2:
-      return yellow2Icon
-    case 0.1:
-      return yellow1Icon
-    default:
-      return yellowIcon
-  }
-}
-
-const getIconForHappiness5 = (answer: number) => {
-  switch (answer) {
-    case 1:
-      return orangeIcon
-    case 0.9:
-      return orange9Icon
-    case 0.8:
-      return orange8Icon
-    case 0.7:
-      return orange7Icon
-    case 0.6:
-      return orange6Icon
-    case 0.5:
-      return orange5Icon
-    case 0.4:
-      return orange4Icon
-    case 0.3:
-      return orange3Icon
-    case 0.2:
-      return orange2Icon
-    case 0.1:
-      return orange1Icon
-    default:
-      return orangeIcon
-  }
-}
-
-const getIconForHappiness6 = (answer: number) => {
-  switch (answer) {
-    case 1:
-      return redIcon
-    case 0.9:
-      return red9Icon
-    case 0.8:
-      return red8Icon
-    case 0.7:
-      return red7Icon
-    case 0.6:
-      return red6Icon
-    case 0.5:
-      return red5Icon
-    case 0.4:
-      return red4Icon
-    case 0.3:
-      return red3Icon
-    case 0.2:
-      return red2Icon
-    case 0.1:
-      return red1Icon
-    default:
-      return redIcon
+      return createColoredIcon(palette[0])
   }
 }
