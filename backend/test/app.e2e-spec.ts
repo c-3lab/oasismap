8687421443f04c45af0dcaf8515676a7e6055483
Sheile <sheile1024@gmail.com>
2025-01-10 14:39:00 +0900

    Temp: Mock AuthService to skip authorization when check /happiness API

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -2,14 +2,28 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { AuthService } from './../src/auth/auth';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  const mockedAuthService = {
+    getUserAttributeFromAuthorization: async (_authorization: string) => {
+      return {
+        nickname: 'dummyNickname',
+        age: '40代',
+        prefecture: '東京都',
+        city: '杉並区',
+      };
+    },
+  };
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideProvider(AuthService)
+      .useValue(mockedAuthService)
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
@@ -20,6 +34,17 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  it('/api/happiness/me (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/api/happiness/me')
+      .query({
+        start: '2025-01-01T00:00:00.000000Z',
+        end: '2025-01-31T23:59:59.999999Z',
+      })
+      .expect(200)
+      .expect({ count: 0, data: [] });
   });
 
   afterAll(async () => {
