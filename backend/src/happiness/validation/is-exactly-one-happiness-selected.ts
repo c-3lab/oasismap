90faccb513677ad37e1782d8687d2d05206c8646
p-hue <p-hue@abelsoft.co.jp>
2025-08-15 16:42:35 +0900

    fix validate DTO

---
+++ backend/src/happiness/validation/is-exactly-one-happiness-selected.ts
@@ -0,0 +1,26 @@
+import { registerDecorator } from 'class-validator';
+
+export function IsExactlyOneHappinessSelected() {
+  return function (object: object, propertyName: string) {
+    registerDecorator({
+      name: 'isExactlyOneHappinessSelected',
+      target: object.constructor,
+      propertyName,
+      validator: {
+        validate(value: any) {
+          if (!value || typeof value !== 'object') {
+            return false;
+          }
+
+          const values = Object.values(value);
+          const countOfOnes = values.filter((val) => val === 1).length;
+
+          return countOfOnes === 1;
+        },
+        defaultMessage() {
+          return 'Exactly one happiness value must be selected (value = 1).';
+        },
+      },
+    });
+  };
+}
