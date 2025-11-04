a4f6a1cd4b0eff760de5f639ef6d986001cb1d3a
ryo-ma <saka_ro@yahoo.co.jp>
2025-11-04 16:19:19 +0900

    Add User-Agent to OSM API request headers

--- backend/src/happiness/happiness-input.service.ts
+++ backend/src/happiness/happiness-input.service.ts
@@ -113,14 +113,24 @@ export class HappinessInputService {
     latitude: number,
     longitude: number,
   ): Promise<Address> {
-    const response = await axios.get(process.env.REVERSE_GEOCODING_URL, {
+    return axios.get(process.env.REVERSE_GEOCODING_URL, {
       params: {
         lat: latitude,
         lon: longitude,
         format: 'geocodejson',
         zoom: 10,
       },
+      headers: {
+        "User-Agent": "OasisMap"
+      }
+    }).then(response => {
+      return response.data.features[0].properties.geocoding.admin;
+    }).catch(error => {
+      console.error('Error in reverse geocoding:', error);
+      return {
+        level4: '',
+        level7: ''
+      };
     });
-    return response.data.features[0].properties.geocoding.admin;
   }
 }
