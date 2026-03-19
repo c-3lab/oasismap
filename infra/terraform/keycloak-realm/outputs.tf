cfc0f1af232bf06a0c71dac4805bacd4b4557edf
orca3d6 <28797462+orca3d6@users.noreply.github.com>
2026-03-19 16:21:35 +0900

    Keycloak クライアントのリダイレクトURIsを出力するよう更新

--- infra/terraform/keycloak-realm/outputs.tf
+++ infra/terraform/keycloak-realm/outputs.tf
@@ -4,3 +4,13 @@ output "realm_id" {
   description = "Keycloak realm id (oasismap)."
   value       = keycloak_realm.oasismap.id
 }
+
+output "general_user_client_redirect_uris" {
+  description = "General user client redirect URIs."
+  value       = keycloak_openid_client.general_user_client.redirect_uris
+}
+
+output "admin_client_redirect_uris" {
+  description = "Admin client redirect URIs."
+  value       = keycloak_openid_client.admin_client.redirect_uris
+}
