3b10987ce022fab7461fc2a27a77f32910ce8b26
orca3d6 <28797462+orca3d6@users.noreply.github.com>
2026-03-23 17:57:17 +0900

    不要なリソースを削除

--- infra/terraform/platform/vnet.tf
+++ infra/terraform/platform/vnet.tf
@@ -7,20 +7,6 @@ resource "azurerm_network_security_group" "dmz" {
   resource_group_name = azurerm_resource_group.main.name
 }
 
-resource "azurerm_network_security_rule" "dmz_https" {
-  name                        = "AllowInternetHTTPSInbound"
-  priority                    = 200
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "443"
-  source_address_prefix       = "Internet"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.main.name
-  network_security_group_name = azurerm_network_security_group.dmz.name
-}
-
 resource "azurerm_network_security_rule" "dmz_gateway_manager" {
   name                        = "AllowGateWayManagerAnyInbound"
   priority                    = 210
@@ -54,20 +40,6 @@ resource "azurerm_network_security_rule" "app_http_from_dmz" {
   resource_group_name         = azurerm_resource_group.main.name
   network_security_group_name = azurerm_network_security_group.app.name
 }
-
-# resource "azurerm_network_security_rule" "app_ssh" {
-#   name                        = "AllowAnySSHInbound"
-#   priority                    = 210
-#   direction                   = "Inbound"
-#   access                      = "Allow"
-#   protocol                    = "Tcp"
-#   source_port_range           = "*"
-#   destination_port_range      = "22"
-#   source_address_prefix       = "*"
-#   destination_address_prefix  = "*"
-#   resource_group_name         = azurerm_resource_group.main.name
-#   network_security_group_name = azurerm_network_security_group.app.name
-# }
 
 resource "azurerm_network_security_group" "agw" {
   name                = "${var.prefix}-NSG-AGW"
